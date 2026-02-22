ad9706025eaddf4b09f9a16c5f442fe48943228d
wanifarooq <wanifarooq848@gmail.com>
2026-02-22 17:54:17 +0000

    macos cpu-only setup file

---
+++ setup_macos.sh
@@ -0,0 +1,5 @@
+pip install torch==2.2.1 torchvision torchaudio
+pip install numpy==1.26
+pip install torch-geometric
+pip install pandas==2.2.3 scikit-learn networkx cleanlab==2.7.1 json-tricks==3.17.3 matplotlib codecarbon scipy requests
+pip install pyyaml
